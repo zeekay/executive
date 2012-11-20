4d1e1b61b9dc751d9fab7f60ea355d1ce13b400a
zeekay <zeekayy@gmail.com>
2012-11-19 21:21:54 -0800

    Added a few comments.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var child_process = require('child_process');
 
-function exec(args, options, callback) {
+function exec(args, callback) {
   var err = '',
       out = '',
       proc;
 
+  // Split arguments from command
   args = args.split(/\s+/g);
   var cmd = args.shift();
 
   if (exec.quiet) {
+    // Do not echo to stdout/stderr
     proc = child_process.spawn(cmd, args);
 
     proc.stdout.on('data', function(data) {
@@ -19,6 +21,7 @@ function exec(args, options, callback) {
       err += data.toString();
     });
   } else {
+    // Echo to stdout/stderr and handle stdin
     process.stdin.resume();
 
     proc = child_process.spawn(cmd, args, {stdio: [process.stdin, process.stdout, process.stderr]});
@@ -38,12 +41,21 @@ function exec(args, options, callback) {
   });
 }
 
+// A couple of global options
 exec.quiet = false;
 exec.safe = true;
 
+// Wrapper function that handles exec being called with only one command or several
 function wrapper(cmds, options, callback) {
   var complete = 0;
 
+  // If options is a function, assume we are called with only two arguments
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+
+  // Default options, callback
   if (options == null) {
     options = {};
   }
@@ -52,14 +64,19 @@ function wrapper(cmds, options, callback) {
     callback = function() {};
   }
 
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
+  // Override defaults if options.quiet or options.safe are specified
+  if (options.quiet != null) {
+    exec.quiet = options.quiet;
   }
 
+  if (options.safe != null) {
+    exec.safe = options.safe;
+  }
+
+  // Iterate over list of cmds, calling each in order as long as all of them return without errors
   function iterate() {
-    exec(cmds[complete], options, function(err, out, code) {
-      if (code !== 0 && exec.safe) {
+    exec(cmds[complete], function(err, out, code) {
+      if (exec.safe && code !== 0) {
         return callback(err, out, code);
       }
 
@@ -72,10 +89,11 @@ function wrapper(cmds, options, callback) {
     });
   }
 
+  // If we are passed an array of commands, call each in serial, otherwise exec immediately.
   if (Array.isArray(cmds)) {
     iterate();
   } else {
-    exec(cmds, options, callback);
+    exec(cmds, callback);
   }
 }
 
