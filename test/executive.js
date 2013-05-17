733ae54b2d24aceda96fd170fccec37d429169da
zeekay <zk@monoid.io>
2013-05-17 13:29:48 -0700

    Update tests.

--- test/executive.js
+++ test/executive.js
@@ -3,14 +3,49 @@ exec = require('../');
 describe('executive', function() {
   describe('bufferedExec', function() {
     it('should buffer all stdout', function(done) {
-      exec.bufferedExec('ls', [], {}, function(err, out, code) {
-        out.should.not.eq('')
+      exec.bufferedExec('zsh', ['-c', 'echo 1'], {}, function(err, out, code) {
+        out.should.eq('1\n')
         done()
       })
     })
 
     it('should buffer all stderr', function(done) {
-      done()
+      exec.bufferedExec('zsh', ['-c', 'doesnotexist'], {}, function(err, out, code) {
+        err.should.contain('command not found')
+        done()
+      })
+    })
+  })
+
+  describe('quietExec', function() {
+    it('should buffer all stdout', function(done) {
+      exec.quietExec('zsh', ['-c', 'echo 1'], {}, function(err, out, code) {
+        out.should.eq('1\n')
+        done()
+      })
+    })
+
+    it('should buffer all stderr', function(done) {
+      exec.quietExec('zsh', ['-c', 'doesnotexist'], {}, function(err, out, code) {
+        err.should.contain('command not found')
+        done()
+      })
+    })
+  })
+
+  describe('interactiveExec', function() {
+    it('should not buffer stdout', function(done) {
+      exec.interactiveExec('zsh', ['-c', 'echo 1'], {}, function(err, out, code) {
+        (out == null).should.be.true
+        done()
+      })
+    })
+
+    it('should not buffer stderr', function(done) {
+      exec.interactiveExec('zsh', ['-c', 'doesnotexist'], {}, function(err, out, code) {
+        (err == null).should.be.true
+        done()
+      })
     })
   })
 })
