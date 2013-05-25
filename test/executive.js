59cfb6b7f6728cf7702fa993e04a10c014548305
Alex Whitman <alex@alexwhitman.com>
2013-05-25 10:25:45 +0100

    Use bash instead of zsh for tests
    bash is more likely to be installed over zsh

--- test/executive.js
+++ test/executive.js
@@ -3,14 +3,14 @@ exec = require('../');
 describe('executive', function() {
   describe('bufferedExec', function() {
     it('should buffer all stdout', function(done) {
-      exec.bufferedExec('zsh', ['-c', 'echo 1'], {}, function(err, out, code) {
+      exec.bufferedExec('bash', ['-c', 'echo 1'], {}, function(err, out, code) {
         out.should.eq('1\n');
         done();
       });
     });
 
     it('should buffer all stderr', function(done) {
-      exec.bufferedExec('zsh', ['-c', 'doesnotexist'], {}, function(err, out, code) {
+      exec.bufferedExec('bash', ['-c', 'doesnotexist'], {}, function(err, out, code) {
         err.should.contain('command not found');
         done();
       });
@@ -19,14 +19,14 @@ describe('executive', function() {
 
   describe('quietExec', function() {
     it('should buffer all stdout', function(done) {
-      exec.quietExec('zsh', ['-c', 'echo 1'], {}, function(err, out, code) {
+      exec.quietExec('bash', ['-c', 'echo 1'], {}, function(err, out, code) {
         out.should.eq('1\n');
         done();
       });
     });
 
     it('should buffer all stderr', function(done) {
-      exec.quietExec('zsh', ['-c', 'doesnotexist'], {}, function(err, out, code) {
+      exec.quietExec('bash', ['-c', 'doesnotexist'], {}, function(err, out, code) {
         err.should.contain('command not found');
         done();
       });
@@ -35,14 +35,14 @@ describe('executive', function() {
 
   describe('interactiveExec', function() {
     it('should not buffer stdout', function(done) {
-      exec.interactiveExec('zsh', ['-c', 'echo 1'], {}, function(err, out, code) {
+      exec.interactiveExec('bash', ['-c', 'echo 1'], {}, function(err, out, code) {
         (out === null).should.equal(true);
         done();
       });
     });
 
     it('should not buffer stderr', function(done) {
-      exec.interactiveExec('zsh', ['-c', 'doesnotexist'], {}, function(err, out, code) {
+      exec.interactiveExec('bash', ['-c', 'doesnotexist'], {}, function(err, out, code) {
         (err === null).should.equal(true);
         done();
       });
